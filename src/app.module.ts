6868b1702653f230ff9d40a29d62a62238960500
arshiapiri <arshia.piri8585@gmail.com>
2023-05-14 00:41:35 +0330

    import typeorm and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserEntity } from './user/user.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: "root",
+      password: "eza3810421",
+      database: "sample2",
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    AuthModule
+  ]
 })
 export class AppModule {}
