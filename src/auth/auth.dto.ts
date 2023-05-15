fb4fe5a502c40eee631c14de5038dd4bbb0e24d0
arshiapiri <arshia.piri8585@gmail.com>
2023-05-15 21:40:21 +0330

    add class logininfodto

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,10 +1,24 @@
-import { IsEmail, IsNotEmpty, MinLength, minLength } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { UserDto } from 'src/user/user.dto';
+import { UserEntity } from 'src/user/user.entity';
 
-export class CreateUserDto {
+export class loginInfoDto {
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
-  @MinLength(8)
   password: string;
+}
+
+export class loginInfoResDto {
+  @Type(() => UserDto)
+  user: UserDto = new UserDto({})
+
+  token: string
+
+  constructor(partial : Partial<UserEntity>, token: string) {
+    Object.assign(this.user, partial)
+    this.token = token 
+}
 }
