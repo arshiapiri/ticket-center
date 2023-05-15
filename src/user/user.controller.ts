27a0dc9b969b7e9b6c54ffbdc78333008983960f
arshiapiri <arshia.piri8585@gmail.com>
2023-05-15 21:42:30 +0330

    get profile add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, Req , UseInterceptors} from '@nestjs/common';
+import { UserService } from './user.service';
+import { Request as RequestType } from 'express';
+import { Response as ResponseType } from 'express';
+import { UserEntity } from './user.entity';
+import { UserDto } from './user.dto';
 
 @Controller('user')
-export class UserController {}
+@UseInterceptors(ClassSerializerInterceptor)
+export class UserController {
+    constructor(private readonly userService: UserService) {
+
+    }
+    @Get()
+    public getProfile(@Req() req:RequestType) : UserDto {
+        const user:UserEntity = req.res.locals.user;
+        return new UserDto(user)
+    }
+    
+}
