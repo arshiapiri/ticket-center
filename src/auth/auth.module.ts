018c6cdeac105629540739eeeb09a8e016388356
arshiapiri <arshia.piri8585@gmail.com>
2023-05-15 21:41:20 +0330

    update module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/user.entity';
+import { sessionEntity } from './auth.entity';
 
 @Module({
-  imports : [UserModule],
+  imports : [TypeOrmModule.forFeature([UserEntity , sessionEntity]) , UserModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports : [AuthService]
 })
 export class AuthModule {}
