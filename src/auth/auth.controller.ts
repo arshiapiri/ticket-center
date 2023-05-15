c02cf0cc63d5e0d7f550061713834c554b7b183a
arshiapiri <arshia.piri8585@gmail.com>
2023-05-15 21:39:54 +0330

    complete login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,46 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateUserDto } from './auth.dto';
+import {
+    Body,
+    ClassSerializerInterceptor,
+    ConflictException,
+    Controller,
+    NotFoundException,
+    Post,
+    UseInterceptors
+} from '@nestjs/common';
+import { loginInfoDto, loginInfoResDto } from './auth.dto';
 import { UserService } from 'src/user/user.service';
 import { UserEntity } from 'src/user/user.entity';
+import { AuthService } from './auth.service';
+import { sessionEntity } from './auth.entity';
+import { CreateUserDto, UserDto } from 'src/user/user.dto';
 
 @Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
-    constructor(private readonly userService : UserService) {}
-    @Post()
-    async registration(@Body() userData : CreateUserDto): Promise<UserEntity> {
-       return this.userService.createUser(userData);
+    constructor(
+        private readonly userService: UserService,
+        private readonly authService: AuthService
+    ) { }
+    @Post("/login")
+    async login(@Body() loginInfo: loginInfoDto): Promise<loginInfoResDto> {
+        const user: UserEntity = await this.userService.findAllByEmail(
+            loginInfo.email
+        )
+        if (!user) throw new NotFoundException("user not found");
+        const token: string = await this.authService.generateToken();
+        const newSession: sessionEntity = await this.authService.createSession(
+            token,
+            user
+            )
+            return new loginInfoResDto(newSession.user , newSession.token)
+    }
+    @Post('/registration')
+    async registration(@Body() userData: CreateUserDto): Promise<UserDto> {
+      const user: UserEntity = await this.userService.findAllByEmail(
+        userData.email,
+      );
+      if (!!user) throw new ConflictException('User already exist');
+      const newUser: UserEntity = await this.userService.createUser(userData);
+      return new UserDto(newUser);
     }
 }
